51e1cefb9f78dbf67fff9ec3fcc23e913c248ee7
cypayne <cypayne@stanford.edu>
2021-12-13 17:06:58 -0800

    add environment specs

---
+++ install.R
@@ -0,0 +1,10 @@
+# QTL analysis
+install.packages(c("qtl"))
+# GO analysis
+install.packages(c("GOstats","GSEABase","biomaRt"))
+# KEGG analysis
+install.packages(c("AnnotationHub","gage","pathview"))
+# DESeq2 DGE analysis, PCA figure
+install.packages(c("tximport","tximportData","GenomicFeatures","readr","DESeq2","PCAtools","ggplot2"))
+# RNAseq heatmap figure
+install.packages(c("DESeq2","genefilter","pheatmap","RColorBrewer","gplots","reshape","ggplot2"))
