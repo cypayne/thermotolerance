14ea47f4df726ff06cba443a0f53b54d1522730d
cypayne <cypayne@stanford.edu>
2022-05-01 16:52:57 -0700

    update R dependencies

--- install.R
+++ install.R
@@ -1,10 +1,14 @@
 # QTL analysis
 install.packages(c("qtl"))
-# GO analysis
-install.packages(c("GOstats","GSEABase","biomaRt"))
-# KEGG analysis
-install.packages(c("AnnotationHub","gage","pathview"))
 # DESeq2 DGE analysis, PCA figure
 install.packages(c("tximport","tximportData","GenomicFeatures","readr","DESeq2","PCAtools","ggplot2"))
 # RNAseq heatmap figure
 install.packages(c("DESeq2","genefilter","pheatmap","RColorBrewer","gplots","reshape","ggplot2"))
+# GO analysis
+install.packages(c("GOstats","GSEABase","biomaRt"))
+# KEGG analysis
+install.packages(c("AnnotationHub","gage","pathview"))
+# Null F1 expression simulations
+install.packages(c("truncnorm"))
+# WGCNA analysis
+install.packages(c("DESeq2", "WGCNA", "tximport","tximportData","GenomicFeatures", "tximportData", "rhdf5"))
